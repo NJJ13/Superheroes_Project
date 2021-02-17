e43c3fb4cb6a5e72e014120ea0164fd581893d08
Nick Johnson <johnsn0913@gmail.com>
2021-02-17 14:55:38 -0600

    Created, Delete, Details, Edit Views in Superhero View folder

--- Controllers/SuperheroController.cs
+++ Controllers/SuperheroController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Superheroes.Data;
+using Superheroes.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +10,59 @@ namespace Superheroes.Controllers
 {
     public class SuperheroController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public SuperheroController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
+            var superheroes = _context.Superheroes;
+            return View(superheroes);
+        }
+        public IActionResult Create()
+        {
             return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Superhero superhero)
+        {
+            _context.Superheroes.Add(superhero);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public IActionResult Delete()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Delete(Superhero superhero)
+        {
+            _context.Superheroes.Remove(superhero);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public IActionResult Read()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Read(Superhero superhero)
+        {
+            _context.Superheroes.Find(superhero);
+            return RedirectToAction("Details");
+        }
+        public IActionResult Edit()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Edit(Superhero superhero)
+        {
+            _context.Superheroes.Update(superhero);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
