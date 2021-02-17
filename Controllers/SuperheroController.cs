50014643870c47e00ea443e64bf119941a5cf647
Nick Johnson <johnsn0913@gmail.com>
2021-02-17 12:10:09 -0600

    init

---
+++ Controllers/SuperheroController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Superheroes.Controllers
+{
+    public class SuperheroController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
